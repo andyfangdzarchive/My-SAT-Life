61d1d8c8d8cb93748a69e3e6820910b909a826d9
Andy Fang <yzmtf2008@gmail.com>
2014-01-31 00:41:57 +0800

    Add list view

--- satlife/grammar/urls.py
+++ satlife/grammar/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<grammar_id>\d+)/$', views.detail, name='detail'),
     url(r'^choose/$', views.choose, name='choose'),
+    url(r'^list/$', views.list),
 )
